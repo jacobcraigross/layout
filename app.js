8fcde8cc591c1d1310cf36757b67e357f269cb1e
Jake Ross <jakeross@Jakes-MacBook-Pro.local>
2017-04-02 10:20:38 -0500

    first node tinkerings

---
+++ app.js
@@ -0,0 +1,16 @@
+// My first time tinkering with Node.js // 
+
+// Created a function that takes data from an array and spits out the average //
+
+function average (scores){
+    var total = 0;
+    scores.forEach (function(score){
+        total += score;
+    });
+    var avg = total/scores.length;
+    return Math.round(avg);
+}
+
+var scores = [60, 90, 99, 44, 88, 77];
+console.log(average(scores));
+
